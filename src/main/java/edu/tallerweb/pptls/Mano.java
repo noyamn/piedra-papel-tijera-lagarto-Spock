f4e2fc97486688c5f8f59e3982e78ee56206bc2c
Noya Mariano <noya.mn@gmail.com>
2013-05-04 01:20:05 -0300

    Modificación del estilo del código

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,50 +1,32 @@
 package edu.tallerweb.pptls;
-
 /**
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
-	
 	private Forma formaJugador;
-	
 	public Mano(final Forma forma) {
-		
-	this.formaJugador=forma;
-	
+		this.formaJugador = forma;
 	}
-
 	/**
 	 * Evaluará el resultado de la partida según las reglas
 	 * del juego.
 	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
-	
 	public Resultado jugarCon(final Mano otra) {
-		
-		
-		
-	int valorJugadorUno=this.formaJugador.getValor();
-	int valorJugadorDos=otra.formaJugador.getValor();
-	
-	if(valorJugadorUno==valorJugadorDos){
-		
+		Integer valorJugadorUno = this.formaJugador.getValor();
+		Integer valorJugadorDos = otra.formaJugador.getValor();
+	if (valorJugadorUno == valorJugadorDos) {
 		return Resultado.EMPATA;
 	}
-	
-	if(valorJugadorUno==(valorJugadorDos+1)%5||valorJugadorUno==(valorJugadorDos+2)%5){
-		
+	if (valorJugadorUno == (valorJugadorDos + 1) % 5 || valorJugadorUno == (valorJugadorDos + 2) % 5) {
 		return Resultado.GANA;
-		
+		}
+	else { return Resultado.PIERDE; }
+		}
 	}
-	else return Resultado.PIERDE;
-		
-	}
-
-}
